4d142a65b9c70ee8cf96fb5261f23d633e3723b8
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-11-08 16:09:04 -0600

    q

--- tester.java
+++ tester.java
@@ -1,5 +1,12 @@
+import java.util.concurrent.CountDownLatch;
+
+import javax.lang.model.util.ElementScanner14;
+
+import java.util.Scanner;
+
 public class tester {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
 
         for (int i = 0; i < 10; i++)
 
@@ -8,5 +15,25 @@ public class tester {
             System.out.print(i);
 
         }
+=======
+        String g1 = "Good Morning!";
+        String g2 = "Good Afternoon!";
+
+        g1.substring()
+        boolean a = false;
+        boolean b = true;
+        int c = 0;
+        int d = 10;
+
+        for(int i = 0; i < 20; i++)
+            if((i > d)) {
+                System.out.print("TRUE : ");
+                System.out.println(i);
+            }
+            else {
+                System.out.print("FALSE : ");
+                System.out.println(i);
+            }
+>>>>>>> Stashed changes
     }
 }
