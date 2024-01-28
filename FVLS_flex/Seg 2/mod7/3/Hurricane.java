f614a9ad2371b0cf9b3c63a60ccb49aa1bc79ad7
JJtarax <93460521+JJtarax@users.noreply.github.com>
2024-01-28 13:48:13 -0500

    .

--- FVLS_flex/Seg 2/mod7/3/Hurricane.java
+++ FVLS_flex/Seg 2/mod7/3/Hurricane.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 /**
  * @purpose: Hurricane Object Class
  *
@@ -5,19 +7,15 @@
  * @version FLVS 2020
  *
  */
- 
- 
-public class Hurricane
-{
+
+public class Hurricane {
    private String name, month;
    private int year;
    private int cat;
    private int pressure;
    private double windspeed;
 
-     
-   
-   //constructor - all private instance variables initialized
+   // constructor - all private instance variables initialized
    public Hurricane(int y, String n, String m, int c, int p, double w) {
       this.year = y;
       this.name = n;
@@ -27,71 +25,163 @@ public class Hurricane
       this.windspeed = w;
    }
 
-   //accessor for year
+   // accessor for year
    public int getYear() {
-      return year; 
+      return year;
    }
 
-   //accessor for name
+   // accessor for name
    public String getName() {
-      return name; 
+      return name;
    }
-   
-   //accessor for month
+
+   // accessor for month
    public String getMonth() {
-      return month;  
+      return month;
    }
-   
-   //accessor for category
+
+   // accessor for category
    public int getCat() {
-      return cat;  
+      return cat;
    }
-   
-   //accessor for pressure
+
+   // accessor for pressure
    public int getPressure() {
       return pressure;
    }
-   
-   //accessor for windspeed
+
+   // accessor for windspeed
    public double getWindspeed() {
       return windspeed;
    }
-   
-   //mutator for year
+
+   // mutator for year
    public void setYear(int y) {
       year = y;
    }
-   
-   //mutator for name
+
+   // mutator for name
    public void setName(String n) {
       name = n;
    }
-   
-   //mutator for month
+
+   // mutator for month
    public void setMonth(String m) {
       month = m;
    }
-   
-   //mutator for category
+
+   // mutator for category
    public void setCat(int c) {
       cat = c;
    }
-   
-   //mutator for pressure
+
+   // mutator for pressure
    public void setPressure(int p) {
       pressure = p;
    }
-   
-   //mutator for windspeed
-   public void setWindspeed (double w) {
+
+   // mutator for windspeed
+   public void setWindspeed(double w) {
       windspeed = w;
    }
 
+   public static String toString(int yearRangeTo, int yearRangeFrom, Hurricane[] hurricanes, int[] categories,
+         int[] years,
+         int[] pressures, double[] windSpeeds) {
+      String result = String.format(
+            "\t\t\tHurricanes %d - %d\nYear\tHurricane\tCategory\tPressure (mb)\tWind Speed (mph)\n", yearRangeFrom,
+            yearRangeTo);
 
-   public String toString() {
-      return month;
-   
-      // complete the toString method using String.format()  
+      DecimalFormat df = new DecimalFormat("0.00");
+      for (Hurricane hurricane : hurricanes) {
+         if (hurricane.getYear() >= yearRangeFrom && hurricane.getYear() <= yearRangeTo) {
+            String name = hurricane.getName();
+            if (name.length() > 7) {
+               result += String.format("%d\t%s\t\t\b\b\b\b\b%1d\t\t %s\t\t %s%n",
+                     hurricane.getYear(),
+                     name, hurricane.getCat(),
+                     hurricane.getPressure() < 1000 ? " " + hurricane.getPressure()
+                           : hurricane.getPressure(),
+                     hurricane.getWindspeed() < 100 ? " " + df.format(hurricane.getWindspeed())
+                           : df.format(hurricane.getWindspeed()));
+            } else {
+               result += String.format("%d\t%s\t\t\t\b\b\b\b\b%1d\t\t %s\t\t %s%n",
+                     hurricane.getYear(),
+                     name, hurricane.getCat(),
+                     hurricane.getPressure() < 1000 ? " " + hurricane.getPressure()
+                           : hurricane.getPressure(),
+                     hurricane.getWindspeed() < 100 ? " " + df.format(hurricane.getWindspeed())
+                           : df.format(hurricane.getWindspeed()));
+            }
+         }
+      }
+      result += "==================\n";
+      return result;
    }
-    
+
+   public static double getAverageValue(double[] array, int[] years, int yearFrom, int yearTo) {
+      double sum = 0;
+      int count = 1;
+      for (int i = 0; i < array.length; i++) {
+         if (yearFrom <= years[i] && yearTo >= years[i]) {
+            sum += array[i];
+            count++;
+         }
+      }
+      return sum / (count - 1);
+   }
+
+   public static double getAverageValue(int[] array, int[] years, int yearFrom,
+         int yearTo) {
+      double sum = 0;
+      int count = 1;
+      for (int i = 0; i < array.length; i++) {
+         if (yearFrom <= years[i] && yearTo >= years[i]) {
+            sum += array[i];
+            count++;
+         }
+      }
+      return sum / (count - 1);
+   }
+
+   public static double getMaxValue(double[] array, int[] years, int yearFrom, int yearTo) {
+      double maxValue = Integer.MIN_VALUE;
+      for (int i = 0; i < array.length; i++) {
+         if (array[i] > maxValue && (yearFrom <= years[i] && yearTo >= years[i])) {
+            maxValue = array[i];
+         }
+      }
+      return maxValue;
+   }
+
+   public static double getMinValue(double[] array, int[] years, int yearFrom, int yearTo) {
+      double minValue = Integer.MAX_VALUE;
+      for (int i = 0; i < array.length; i++) {
+         if (array[i] < minValue && (yearFrom <= years[i] && yearTo >= years[i])) {
+            minValue = array[i];
+         }
+      }
+      return minValue;
+   }
+
+   public static int getMaxValue(int[] array, int[] years, int yearFrom, int yearTo) {
+      int maxValue = Integer.MIN_VALUE;
+      for (int i = 0; i < array.length; i++) {
+         if (array[i] > maxValue && (yearFrom <= years[i] && yearTo >= years[i])) {
+            maxValue = array[i];
+         }
+      }
+      return maxValue;
+   }
+
+   public static int getMinValue(int[] array, int[] years, int yearFrom, int yearTo) {
+      int minValue = Integer.MAX_VALUE;
+      for (int i = 0; i < array.length; i++) {
+         if (array[i] < minValue && (years[i] >= yearFrom && years[i] <= yearTo)) {
+            minValue = array[i];
+         }
+      }
+      return minValue;
+   }
+
 }
--- FVLS_flex/Seg 2/mod7/3/HurricaneTester.java
+++ FVLS_flex/Seg 2/mod7/3/HurricaneTester.java
@@ -1,12 +1,13 @@
 
 /**
  * Starter code for the Hurricane Tester
- * APCS Team 2020
+ * Tejas Upadhyay
  *
  */
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import java.time.Year;
 import java.io.File;
 import java.util.Scanner;
@@ -16,10 +17,10 @@ public class HurricaneTester
 {
 
     public static void main(String[] args) throws IOException {
+
         // read data from text file & put in an array
         File fileName = new File("HurricaneData.txt");
         Scanner inFile = new Scanner(fileName);
-        Scanner in = new Scanner(System.in);
         int numValues = 0;
 
         // count number of lines in text file
@@ -72,11 +73,13 @@ public class HurricaneTester
         }
 
         // user prompted for range of years
+        Scanner in = new Scanner(System.in);
         System.out.println("Please enter starting year");
         int yearRangeFrom = in.nextInt();
         System.out.println("Please enter end year");
         int yearRangeTo = in.nextInt();
         in.close();
+
         if (yearRangeTo < yearRangeFrom) {
             System.out.println("Your starting year needs to be less then your end year");
             return;
@@ -86,11 +89,50 @@ public class HurricaneTester
                     "The data that we have range of is from " + years[0] + " to " + years[years.length - 1] + "\n");
             return;
         }
-
-        // print the data
-        System.out.println("\t\t\tHurricanes " + yearRangeFrom + " - " + yearRangeTo);
-        System.out.println("""
-                Year\tHurricane\tCategory\tPressuure (mb)\tWind Speed (mph)
-                ========================================================================""");
+        System.out.println(
+                Hurricane.toString(yearRangeTo, yearRangeFrom, hurricanes, categories, years, pressures, windSpeeds));
+        // // print the data
+        // System.out.println("\t\t\tHurricanes " + yearRangeFrom + " - " +
+        // yearRangeTo);
+        // System.out.println("""
+        // Year\tHurricane\tCategory\tPressuure (mb)\tWind Speed (mph)
+        // ========================================================================""");
+        // DecimalFormat df = new DecimalFormat("0.00");
+        // for (Hurricane hurricane : hurricanes) {
+        // if (hurricane.getYear() >= yearRangeFrom && hurricane.getYear() <=
+        // yearRangeTo) {
+        // if (hurricane.getName().length() > 7)
+        // System.out.printf("%d\t%s\t\t\b\b\b\b\b%1d\t\t %s\t\t %s%n",
+        // hurricane.getYear(),
+        // hurricane.getName(), hurricane.getCat(),
+        // hurricane.getPressure() < 1000 ? " " + hurricane.getPressure()
+        // : hurricane
+        // .getPressure(),
+        // hurricane.getWindspeed() < 100 ? " " + df.format(hurricane.getWindspeed())
+        // : df.format(hurricane.getWindspeed()));
+        // else
+        // System.out.printf("%d\t%s\t\t\t\b\b\b\b\b%1d\t\t %s\t\t %s%n",
+        // hurricane.getYear(),
+        // hurricane.getName(), hurricane.getCat(),
+        // hurricane.getPressure() < 1000 ? " " + hurricane.getPressure()
+        // : hurricane
+        // .getPressure(),
+        // hurricane.getWindspeed() < 100 ? " " + df.format(hurricane.getWindspeed())
+        // : df.format(hurricane.getWindspeed()));
+        // }
+        // }
+        // System.out.println("========================================================================");
+        // System.out.printf("\tAverage: %13.1f %18.1f %14.2f%n",
+        // Hurricane.getAverageValue(categories, years, yearRangeFrom, yearRangeTo),
+        // Hurricane.getAverageValue(pressures, years, yearRangeFrom, yearRangeTo),
+        // Hurricane.getAverageValue(windSpeeds, years, yearRangeFrom, yearRangeTo));
+        // System.out.printf("\tMinimum: %11d %18d %16.2f%n",
+        // Hurricane.getMinValue(categories, years, yearRangeFrom, yearRangeTo),
+        // Hurricane.getMinValue(pressures, years, yearRangeFrom, yearRangeTo),
+        // Hurricane.getMinValue(windSpeeds, years, yearRangeFrom, yearRangeTo));
+        // System.out.printf("\tMaximum: %11d %18d %16.2f%n",
+        // Hurricane.getMaxValue(categories, years, yearRangeFrom, yearRangeTo),
+        // Hurricane.getMaxValue(pressures, years, yearRangeFrom, yearRangeTo),
+        // Hurricane.getMaxValue(windSpeeds, years, yearRangeFrom, yearRangeTo));
     }
 }
