0f445e5d52eafce7dd5c8e8a28a32c89cbd1ac78
JJtarax <93460521+JJtarax@users.noreply.github.com>
2024-02-01 18:17:37 -0500

    .

--- FVLS_flex/Seg 2/mod7/2/CheeseCakeTesterV1.java
+++ FVLS_flex/Seg 2/mod7/2/CheeseCakeTesterV1.java
@@ -1,3 +1,4 @@
+
 /**
  * @purpose
  *
@@ -7,30 +8,26 @@
  */
 import java.util.ArrayList;
 
-public class CheeseCakeTesterV1
-{
-    public static void main(String[] args)
-    {
-        //initializing and declareing an ArrayList and adding in objects as elements
+public class CheeseCakeTesterV1 {
+    public static void main(String[] args) {
+        // initializing and declareing an ArrayList and adding in objects as elements
         ArrayList<CheeseCakeV1> cake = new ArrayList<CheeseCakeV1>();
-            // add CheeseCake to the ArrayList
-        
-        //calls several method for each object to perform calculations
-        for(CheeseCakeV1 dataRecord : cake)
-        {
+        // add CheeseCake to the ArrayList
+
+        // calls several method for each object to perform calculations
+        for (CheeseCakeV1 dataRecord : cake) {
             // invoke methods
         }
-        
-        //printing the format of the table
+
+        // printing the format of the table
         System.out.println("|       Cheese Cake Data           |             Ingredient Calculations             |");
-        System.out.println("| Index | Quantity |  Flavor       | Cream Cheese | Servings |   Sugar   |  Vanilla  |");
-        System.out.println("|-------|----------|---------------|--------------|----------|-----------|-----------|");
+        System.out.println(" | Index | Quantity |  Flavor       | Cream Cheese | Servings |   Sugar   |  Vanilla  |");
+        System.out.println(" |-------|----------|---------------|--------------|----------|-----------|-----------|");
 
         CheeseCakeV1 dataRecord;
-        
-        //using a for loop to print out the objects' data
-        for(int index = 0; index < cake.size(); index ++)
-        {
+
+        // using a for loop to print out the objects' data
+        for (int index = 0; index < cake.size(); index++) {
             // print each CheeseCake's data
         }
     }
--- FVLS_flex/Seg 2/mod7/2/CheeseCakeV1.java
+++ FVLS_flex/Seg 2/mod7/2/CheeseCakeV1.java
@@ -1,109 +1,115 @@
 /**
  * @purpose
  *
- * @author (enter your name)
+ * @author Tejas Upadhyay
  * @version (enter today's date)
  *
  */
-public class CheeseCakeV1
-{
+public class CheeseCakeV1 {
     private int myQuantity, myServings, myCreamCheese, myVanilla;
     private double mySugar;
     private String myFlavor;
-    
-   /**
-    * Constructor for objects of type CheeseCakeV1
-    * @param flavor
-    * @param quantity
-    */
-    CheeseCakeV1(String flavor,int quantity)
-    {
-        //******* fill in code for constructor here ****//
+
+    /**
+     * Constructor for objects of type CheeseCakeV1
+     * 
+     * @param flavor
+     * @param quantity
+     */
+    CheeseCakeV1(String flavor, int quantity) {
+        // ******* fill in code for constructor here ****//
+        this.myFlavor = flavor;
+        this.myQuantity = quantity;
     }
 
     /**
      * Mutator method to calculate the number of servings
      */
-    public void calcTotalServings()
-    {
-        //******* fill in code for method here - 16 servings per cake ****//
+    public void calcTotalServings() {
+        // ******* fill in code for method here - 16 servings per cake ****//
+        int ser = 0;
+        for (int i = 0; i >= this.myQuantity; i++) {
+            ser += 16;
+        }
     }
 
     /**
      * Mutator method to calculate the Cream Cheese Needed
      */
-    public void calcCreamCheese()
-    {
-        //******* fill in rest of method here - 32 ounces per cake ****//
+    public void calcCreamCheese() {
+        // ******* fill in rest of method here - 32 ounces per cake ****//
+        int CreamCheese = 0;
+        for (int i = 0; i >= this.myQuantity; i++) {
+            CreamCheese += 32;
+        }
     }
 
     /**
      * Mutator method to calculate the Vanilla Needed
      */
-    public void calcVanilla()
-    {
-        //******* fill in rest of method here - 1 teaspoon per cake ****//
+    public void calcVanilla() {
+        // ******* fill in rest of method here - 1 teaspoon per cake ****//
+        int Vaniila = 0;
+        for (int i = 0; i >= this.myQuantity; i++) {
+            Vaniila += 1;
+        }
     }
 
     /**
      * Mutator method to calculate the Sugar Needed
      */
-    public void calcSugar()
-    {
-        //******* fill in rest of method here - 1/3 cup per cake ****//
-    }    
+    public void calcSugar() {
+        // ******* fill in rest of method here - 1/3 cup per cake ****//
+        double Sugar = 0;
+        for (int i = 0; i >= this.myQuantity; i++) {
+            Sugar += 1 / 3;
+        }
+    }
 
     /**
      * Getter method to return the number of cakes (no parameters)
      */
-    public int getquantity()
-    {
+    public int getquantity() {
         return myQuantity;
     }
 
     /**
      * Getter method to return flavor of the cake (no parameters)
      */
-    public String getFlavor()
-    {
+    public String getFlavor() {
         return myFlavor;
     }
 
     /**
      * Getter method to return amount of Sugar needed (no parameters)
      */
-    public double getSugar()
-    {
+    public double getSugar() {
         return mySugar;
     }
 
     /**
      * Getter method to return amount of Cream Cheese needed (no parameters)
      */
-    public int getCreamCheese()
-    {
+    public int getCreamCheese() {
         return myCreamCheese;
     }
 
     /**
      * Getter method to return amount of Vanilla needed (no parameters)
      */
-    public int getVanilla()
-    {
+    public int getVanilla() {
         return myVanilla;
     }
 
     /**
      * Getter method to return number of Servings (no parameters)
      */
-    public int getServings()
-    {
+    public int getServings() {
         return myServings;
     }
-    
-    public String toString()
-    {
-        //******* Practice your printf() skills by formatting this data! ****//
+
+    public String toString() {
+        // ******* Practice your printf() skills by formatting this data! ****//
         return String.format("The data that matches the headings in the CheeseCakeV1 file");
     }
 }
