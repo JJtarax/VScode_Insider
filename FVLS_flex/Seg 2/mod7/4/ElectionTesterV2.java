8d2d216866e1cd6e3721398844af652aed50eacb
JJtarax <tejasupadhyayfl@gmail.com>
2024-02-04 21:10:56 -0500

    .

--- FVLS_flex/Seg 2/mod7/4/ElectionTesterV2.java
+++ FVLS_flex/Seg 2/mod7/4/ElectionTesterV2.java
@@ -6,6 +6,15 @@ public class ElectionTesterV2 {
 
         // add Blueberry to the ArrayList
         Candidate ts = new Candidate("Tony Stark", 3691);
+        Candidate hp = new Candidate("Henry Pym", 2691);
+        Candidate bb = new Candidate("B____ B_____", 1962);
+        Candidate sr = new Candidate("S____ R_____", 1491);
+        Candidate cd = new Candidate("C____ D______", 1968);
+
         cakes.add(ts);
+        cakes.add(hp);
+        cakes.add(bb);
+        cakes.add(sr);
+        cakes.add(cd);
     }
 }
